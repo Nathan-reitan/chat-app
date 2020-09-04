56a0ae4bd1dfb0f4f9364055c924a58526a07c5b
Nathan Reitan <nathanreitan@gmail.com>
2020-09-04 13:05:00 -0700

    unnecessary component files removed

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header className="text-light bg-dark d-flex justify-content-between">
-      <span>$ Wicked Sales</span>
+      <span>Reitan Chat App</span>
       <span className="mx-2">
         <i onClick={() => props.setView('cart')} className="fas fa-shopping-cart mx-2 pointer"></i>
         {props.cartItemCount}
